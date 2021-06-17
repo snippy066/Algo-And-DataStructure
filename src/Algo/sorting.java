7b6e553821da41b354fd29b321382f86a5aa2401
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-17 18:30:13 +0530

    selection sort

--- src/Algo/sorting.java
+++ src/Algo/sorting.java
@@ -1,18 +1,27 @@
 import java.util.*;
 
 public class sorting {
-    private static StringBuilder sb=new StringBuilder();
+    //private static StringBuilder sb=new StringBuilder();
     public static void main(String ar[]){
         Scanner in=new Scanner(System.in);
         int n=in.nextInt();
         int[] arr=new int[n];
         for(int i=0;i<n;i++) arr[i]=in.nextInt();
-        selectionSort(arr);
-        System.out.println(sb);
+        selectionSort(arr,n);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static void selectionSort(int [] arr){
-
+    public static void selectionSort(int [] arr,int len){
+        for(int i=0;i<len-1;i++){
+            int val=i;
+            for(int j=i+1;j<len;j++){
+                if(arr[i]>arr[j])
+                    val=j;
+            }
+            int tem=arr[i];
+            arr[i]=arr[val];
+            arr[val]=tem;
+        }
     }
 }
 
