1911c4398ca6eeecb31e3319840eb8294bc8086b
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-17 18:44:10 +0530

    Update insertionSort.java

--- src/Algo/insertionSort.java
+++ src/Algo/insertionSort.java
@@ -1,4 +1,18 @@
 package Algo;
+import java.util.*;
 
 public class insertionSort {
+    public static void main(String ar[]){
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr= new int[n];
+        for(int i=0;i<n;i++)  arr[i]=sc.nextInt();
+
+        sortAsc(arr,n);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sortAsc(int[] arr,int siz){
+        
+    }
 }
