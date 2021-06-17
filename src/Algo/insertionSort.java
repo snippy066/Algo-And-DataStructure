66ffae791c872d4e32e05368650386118cfbab07
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-17 18:48:54 +0530

    Update insertionSort.java

--- src/Algo/insertionSort.java
+++ src/Algo/insertionSort.java
@@ -13,6 +13,15 @@ public class insertionSort {
     }
 
     public static void sortAsc(int[] arr,int siz){
-        
+        for(int i=0;i<siz;i++){
+            int swap=arr[i];
+            int j=i-1;
+
+            while(j>=0 && arr[j]>swap){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=swap;
+        }
     }
 }
