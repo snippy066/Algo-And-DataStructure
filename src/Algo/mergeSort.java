54a52f6ea36f011985113d44b8e3e67f470b4671
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-17 18:52:10 +0530

    Update mergeSort.java

--- src/Algo/mergeSort.java
+++ src/Algo/mergeSort.java
@@ -1,4 +1,17 @@
 package Algo;
+import java.util.*;
 
 public class mergeSort {
+    public static void main(String ar[]){
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr= new int[n];
+        for(int i=0;i<n;i++)  arr[i]=sc.nextInt();
+
+        sortAsc(arr,n);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void sortAsc(int[] arr,int siz) {
+
+    }
 }
