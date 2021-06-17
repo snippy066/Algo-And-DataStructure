2e83a0529705376f13cb38e757310d176f8668e1
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-17 18:19:09 +0530

    selection sort using java

--- src/Algo/sorting.java
+++ src/Algo/sorting.java
@@ -1,8 +1,18 @@
 import java.util.*;
 
 public class sorting {
+    private static StringBuilder sb=new StringBuilder();
     public static void main(String ar[]){
-        
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++) arr[i]=in.nextInt();
+        selectionSort(arr);
+        System.out.println(sb);
+    }
+
+    public static void selectionSort(int [] arr){
+
     }
 }
 
