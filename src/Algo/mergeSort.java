f0999cfa3926ba04ab2b61763b4dff223efc45f6
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-17 19:47:08 +0530

    Update mergeSort.java

--- src/Algo/mergeSort.java
+++ src/Algo/mergeSort.java
@@ -8,10 +8,21 @@ public class mergeSort {
         int[] arr= new int[n];
         for(int i=0;i<n;i++)  arr[i]=sc.nextInt();
 
-        sortAsc(arr,n);
+        div(arr,0,n);
         System.out.println(Arrays.toString(arr));
     }
-    public static void sortAsc(int[] arr,int siz) {
+    public static void div(int[] arr,int l,int r) {
+        if(l<r){
+            int m=(l+r)/2;
 
+            div(arr,l,m);
+            div(arr,m+1,r);
+
+            merge(arr,l,m,r);
+        }
+    }
+
+    public static void merge(int[] arr, int l, int m, int r){
+        
     }
 }
