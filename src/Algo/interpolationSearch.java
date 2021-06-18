11fdf7be01fd842b90abaf7ad3f106c6489a8df2
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-18 21:18:29 +0530

    Update interpolationSearch.java

--- src/Algo/interpolationSearch.java
+++ src/Algo/interpolationSearch.java
@@ -1,4 +1,24 @@
 package Algo;
+import java.util.*;
+
 
 public class interpolationSearch {
+    public static void main(String ar[]) {
+
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();     //array length
+
+    int[] arr = new int[n];
+
+    for(
+    int i = 0;
+    i<n;i++)arr[i]=in.nextInt();   //sorted array
+
+    int key = in.nextInt();   //which needs to be searched
+
+    int val = interpol(arr, 0, n, key);
+        System.out.println(val);
+    }
+
+    
 }
