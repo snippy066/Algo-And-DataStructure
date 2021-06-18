87b9bdbba7462c048325d743ffa937a6fb385457
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-18 21:24:25 +0530

    Update interpolationSearch.java

--- src/Algo/interpolationSearch.java
+++ src/Algo/interpolationSearch.java
@@ -20,5 +20,17 @@ public class interpolationSearch {
         System.out.println(val);
     }
 
-    
+    public static int interpol(int arr[],int lo,int hi,int k){
+        int pos=-1;
+        if(lo<=hi && arr[lo]<=k && arr[hi]>=k){
+            pos=lo+((hi-lo)/(arr[hi]-arr[lo]))*(k-arr[lo]);
+
+            if(arr[pos]==k)
+                return pos+1;
+            else if(arr[pos]>k)
+                return interpol(arr,lo,pos-1,k);
+            return interpol(arr,pos+1,hi,k);
+        }
+        return -1;
+    }
 }
