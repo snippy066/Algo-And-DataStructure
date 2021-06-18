445523a11019312ea106aca9bf9f0c5e9b23091a
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-18 21:46:01 +0530

    Update expoSearch.java

--- src/Algo/expoSearch.java
+++ src/Algo/expoSearch.java
@@ -1,10 +1,9 @@
 package Algo;
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class expoSearch {
     public static void main(String ar[]) {
-
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();     //array length
 
@@ -14,7 +13,18 @@ public class expoSearch {
 
         int key = in.nextInt();   //which needs to be searched
 
-        int val = interpol(arr, 0, n-1, key);
+        int val = exposrc(arr,n, key);
         System.out.println(val);
+    }
+
+    public static int exposrc(int[] arr,int siz,int k){
+        int i=1;
+        if(arr[0]==k)
+            return i;
+
+        while(i<siz && arr[i]<=k)
+            i*=2;
+
+        return Arrays.binarySearch(arr,i/2,Math.min(i,siz-1),k);
     }
 }
