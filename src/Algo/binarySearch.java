bc0aff37c2b4e0662f87b3cf88b183159ae80f4e
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-18 15:58:07 +0530

    Update binarySearch.java

--- src/Algo/binarySearch.java
+++ src/Algo/binarySearch.java
@@ -1,4 +1,31 @@
 package Algo;
+import java.util.*;
 
 public class binarySearch {
+    public static void main(String ar[]){
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();     //array length
+        int key=in.nextInt();   //which needs to be searched
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++)  arr[i]=in.nextInt();
+
+        Arrays.sort(arr);
+
+        int val=binsrc(arr,0,n,key);
+        System.out.println(val);
+    }
+
+    public static int binsrc(int[] arr,int r,int l,int k){
+        if(r<l){
+            int mid=(r+l)/2;
+            if(arr[mid]==k)
+                return mid+1;
+            else if(arr[mid]>k)
+                return binsrc(arr,r,mid-1,k);
+            return binsrc(arr,mid+1,l,k);
+        }
+        else
+            return -1;
+    }
 }
