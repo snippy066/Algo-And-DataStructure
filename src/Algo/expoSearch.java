bdd7724e8a5213823f42c8da9119a0023a7ad3ff
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-18 21:33:50 +0530

    Update expoSearch.java

--- src/Algo/expoSearch.java
+++ src/Algo/expoSearch.java
@@ -1,4 +1,20 @@
 package Algo;
 
+import java.util.Scanner;
+
 public class expoSearch {
+    public static void main(String ar[]) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();     //array length
+
+        int[] arr = new int[n];
+
+        for(int i = 0;i<n;i++) arr[i]=in.nextInt();   //sorted array
+
+        int key = in.nextInt();   //which needs to be searched
+
+        int val = interpol(arr, 0, n-1, key);
+        System.out.println(val);
+    }
 }
