5c18326dc1cff46142fbcce202d13ec811dd4735
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-19 21:36:10 +0530

    merge Sort using java

---
+++ src/Algo/sorting/mergeSort.java
@@ -0,0 +1,58 @@
+package Algo.sorting;
+import java.util.*;
+
+public class mergeSort {
+    public static void main(String ar[]){
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr= new int[n];
+        for(int i=0;i<n;i++)  arr[i]=sc.nextInt();
+
+        div(arr,0,n-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void div(int[] arr,int l,int r) {
+        if(l<r){
+            int m=(l+r)/2;
+
+            div(arr,l,m);
+            div(arr,m+1,r);
+
+            merge(arr,l,m,r);
+        }
+    }
+
+    public static void merge(int[] arr, int l, int m, int r){
+        int fir=m-l+1;  //size of first arr[l,m]
+        int sec=r-m;    //size of second arr[m+1,r]
+
+        int left[] =new int[fir];
+        int right[] =new int[sec];
+
+        for(int i=0;i<fir;i++)  left[i]=arr[l+i];
+        for(int i=0;i<sec;i++)  right[i]=arr[m+i+1];
+
+        int k=l,i=0,j=0;
+        while(i<fir && j<sec){
+            if(left[i]<=right[j]){
+                arr[k]=left[i];
+                i++;
+            }
+            else{
+                arr[k]=right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i<fir){
+            arr[k]=left[i];
+            i++; k++;
+        }
+        while(j<sec){
+            arr[k]=right[j];
+            j++; k++;
+        }
+
+    }
+}
