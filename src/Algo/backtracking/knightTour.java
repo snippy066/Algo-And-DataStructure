85a9f27809e54545d555973e78713ae4632b4020
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-19 22:19:34 +0530

    Update knightTour.java

--- src/Algo/backtracking/knightTour.java
+++ src/Algo/backtracking/knightTour.java
@@ -3,12 +3,13 @@ import javax.swing.*;
 import java.util.*;
 
 public class knightTour {
+    private static int siz;
     private static int[][] sol;
     public static void main(String ar[]){
         Scanner in=new Scanner(System.in);
         int n=in.nextInt();
         solvKt(n);
-
+        siz=n;
     }
 
     public static void solvKt(int n){
@@ -24,16 +25,29 @@ public class knightTour {
 
         sol[0][0]=0;
 
-        if(!solext(0,0,1,sol,n,xmove,ymove))   //(0,0) starting co-ordinate and 1 for move refrence
+        if(!solext(0,0,1,sol,xmove,ymove))   //(0,0) starting co-ordinate and 1 for move refrence
             System.out.println("sol doesn't exists");
         else
             solprint(sol,n);
     }
 
-    static boolean solext(int x,int y,int move,int[][] sol,int siz,int[] xmo,int[] ymov){
+    static boolean solext(int x,int y,int move,int[][] sol,int[] xmo,int[] ymov){
+
+        if(move==siz*siz)
+            return true;
+        for(int i=0;i<siz;i++){
+            int nex_xmov=x+xmo[i];
+            int nex_ymov=y+ymov[i];
+
+            if((nex_xmov>=0 && nex_xmov<siz && nex_ymov>=0
+                    && nex_ymov<siz && sol[nex_xmov][nex_ymov]==-1 )){
+                sol[nex_xmov][nex_ymov]=move;
+                if(solext(nex_xmov,nex_ymov,move+1,sol,xmo,ymov));
+            }
+        }
         return false;
     }
-    
+
     static void solprint(int[][] arr,int n){
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++)
