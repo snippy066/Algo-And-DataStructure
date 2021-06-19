3ea9dcb598b566aade7818e9655a0a67ef378cd8
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-19 21:37:13 +0530

    exponential search using java

---
+++ src/Algo/searching/expoSearch.java
@@ -0,0 +1,30 @@
+package Algo.searching;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class expoSearch {
+    public static void main(String ar[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();     //array length
+
+        int[] arr = new int[n];
+
+        for(int i = 0;i<n;i++) arr[i]=in.nextInt();   //sorted array
+
+        int key = in.nextInt();   //which needs to be searched
+
+        int val = exposrc(arr,n, key);
+        System.out.println(val);
+    }
+
+    public static int exposrc(int[] arr,int siz,int k){
+        int i=1;
+        if(arr[0]==k)
+            return i;
+
+        while(i<siz && arr[i]<=k)
+            i*=2;
+
+        return Arrays.binarySearch(arr,i/2,Math.min(i,siz-1),k);
+    }
+}
