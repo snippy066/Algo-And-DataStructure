a5b0083491cb0727ab9c7508e37f18ae98b6828b
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-19 22:00:51 +0530

    Create knightTour.java

---
+++ src/Algo/backtracking/knightTour.java
@@ -0,0 +1,35 @@
+package Algo.backtracking;
+import javax.swing.*;
+import java.util.*;
+
+public class knightTour {
+    private static int[][] sol;
+    public static void main(String ar[]){
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        solvKt(n);
+
+    }
+
+    public static void solvKt(int n){
+        sol=new int[n][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++)
+                sol[i][j]=-1;
+        }
+
+        int[] xmove={2,2,-2,-2,1,1,-1,-1};
+        int[] ymove={1,-1,1,-1,2,-2,2,-2};
+
+        sol[0][0]=0;
+
+        if(!solext(0,0,1,sol,n,xmove,ymove));   //(0,0) starting co-ordinate and 1 for move refrence
+            System.out.println("sol doesn't exists");
+
+        else
+            solprint(sol);
+    }
+
+    
+}
