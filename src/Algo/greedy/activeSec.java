0c95f544834377ccd1543b4996f87f39d397e762
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-06-20 23:50:27 +0530

    Update activeSec.java

--- src/Algo/greedy/activeSec.java
+++ src/Algo/greedy/activeSec.java
@@ -2,8 +2,24 @@ package Algo.greedy;
 import java.util.*;
 
 public class activeSec {
+    private static StringBuilder sb;
     public static void main(String ar[]){
         Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] start =new int[n];
+        int[] end=new int[n];
 
+        for(int i=0;i<n;i++)     start[i]=sc.nextInt();
+        for(int i=0;i<n;i++)     end[i]=sc.nextInt();
+
+        sb=new StringBuilder();
+        sb.append(0+" ");
+        int i=0;
+        for(int j=1;j<n;j++){
+            if(start[j]>=end[i]){
+                sb.append(j+" ");
+                i=j;
+            }
+        }
     }
 }
