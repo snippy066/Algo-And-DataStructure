9016bb0c18bf3f5098b6bb840a35d0b6c6208335
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-09 18:42:26 +0530

    Update differenceRangeUpd.java

--- src/DS/arrays/differenceRangeUpd.java
+++ src/DS/arrays/differenceRangeUpd.java
@@ -1,4 +1,36 @@
 package DS.arrays;
 
+import com.sun.org.apache.xpath.internal.operations.String;
+
+import java.util.Scanner;
+
 public class differenceRangeUpd {
+    private static StringBuilder sb=new StringBuilder();
+    public static void main(String ar[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+        int[] d=new int[n+1]; //difference array
+
+        for(int i=0;i<n;i++)  arr[i]=sc.nextInt();
+        int t=sc.nextInt();
+
+        initArr(d,arr);
+        while(t-->0) {
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+            int val = sc.nextInt();
+
+            updateArr(d, l, r, val);
+            sb.append("\n");
+        }
+
+    }
+    static void initArr(int[] arr1,int[] orArr){
+           
+    }
+    static void updateArr(int[] arr,int l,int r,int val){
+
+    }
 }
