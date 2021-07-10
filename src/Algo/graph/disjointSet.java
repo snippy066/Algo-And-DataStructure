b164b587eb9c9b4d2c2e0d571d5bed0ae6e05cf0
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-10 22:30:16 +0530

    Update disjointSet.java

--- src/Algo/graph/disjointSet.java
+++ src/Algo/graph/disjointSet.java
@@ -3,23 +3,32 @@ package Algo.graph;
 import java.util.Scanner;
 
 public class disjointSet {
-    public static void main(String ar[]){
-        Scanner sc=new Scanner(System.in);
+    public static void main(String ar[]) {
+        Scanner sc = new Scanner(System.in);
 
-        int v=sc.nextInt();
-        int e=sc.nextInt();
+        int v = sc.nextInt();
+        int e = sc.nextInt();
 
-        Graph gr=new Graph(v,e);
-        String[] s=sc.nextLine().split(" ");
-        for(int i=0;i<e;i++){
-            gr.edge[i].src=s[i];
-            gr.edge[i].des=s[i+1];
+        Graph gr = new Graph(v, e);
+        String[] s = sc.nextLine().split(" ");
+        for (int i = 0; i < e; i++) {
+            gr.edge[i].src = s[i];
+            gr.edge[i].des = s[i + 1];
         }
 
-        if(isCycle(gr)==true)
+        if (isCycle(gr) == true)
             System.out.println("graph contains cycle");
         else
             System.out.println("graph doesn't contain cycle");
 
     }
+}
+
+class Graph{
+    int E ,V;
+    Edge edge[];
+
+    class Edge{
+        int src,des;
+    };
 }
