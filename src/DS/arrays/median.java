b676b06d47b65c3be1980788e285d9e3589192a2
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-12 20:30:49 +0530

    Update median.java

--- src/DS/arrays/median.java
+++ src/DS/arrays/median.java
@@ -19,7 +19,8 @@ public class median {
     }
 
     static void calMedi(int[] arr, int len){
-        PriorityQueue<Integer> sml = new PriorityQueue<Integer>(Collections.reverseOrder());
+        PriorityQueue<Integer> sml = new PriorityQueue<Integer>();
+        
         PriorityQueue<Integer> grt=new PriorityQueue<>();
 
         sml.add(arr[0]);
