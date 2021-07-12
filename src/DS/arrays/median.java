ce502fd353c61ae951febd8dea9a0f77b5a2a273
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-12 19:37:44 +0530

    Update median.java

--- src/DS/arrays/median.java
+++ src/DS/arrays/median.java
@@ -1,2 +1,17 @@
-package DS.arrays;public class median {
+package DS.arrays;
+
+import java.util.Scanner;
+
+public class median {
+    public static void main(String ar[]){
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++)    arr[i]=sc.nextInt();
+
+        calMedi(arr,n);
+    }
 }
