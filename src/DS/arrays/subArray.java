8bbd3e2e5abe0ff093ee90ba997255313e4e2904
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-13 14:53:58 +0530

    Update subArray.java

--- src/DS/arrays/subArray.java
+++ src/DS/arrays/subArray.java
@@ -14,4 +14,19 @@ public class subArray {
 
         maxArraySum(arr,n);
     }
+
+    static void maxArraySum(int[] arr,int len){
+        int maxhere=0,maxSofar=0;
+
+        for(int i=0;i<len;i++){
+            maxhere+=arr[i];
+
+            if(maxSofar<maxhere)
+                    maxSofar=maxhere;
+            if(maxhere<0)
+                maxhere=0;
+        }
+        System.out.println(maxSofar);
+        
+    }
 }
