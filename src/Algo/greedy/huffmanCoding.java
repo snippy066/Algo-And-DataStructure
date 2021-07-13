39f6e26ad7b5f0fa9690da6d61b28c0519484171
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-13 13:52:28 +0530

    Update huffmanCoding.java

--- src/Algo/greedy/huffmanCoding.java
+++ src/Algo/greedy/huffmanCoding.java
@@ -1,4 +1,18 @@
 package Algo.greedy;
 
+import java.util.Scanner;
+
 public class huffmanCoding {
+    public static void main(String ar[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        sc.nextLine();
+
+        String[] ch=sc.nextLine().split(" ");
+
+        int[] freq=new int[n];
+        for(int i=0;i<n;i++)  freq[i]=sc.nextInt();
+
+        buildHuff(ch,freq,n);
+    }
 }
