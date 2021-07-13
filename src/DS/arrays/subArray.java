ef440dd9f91f99d869c6c33e8f822022194cd326
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-13 14:51:57 +0530

    Update subArray.java

--- src/DS/arrays/subArray.java
+++ src/DS/arrays/subArray.java
@@ -1,4 +1,17 @@
 package DS.arrays;
 
+import java.util.Scanner;
+
 public class subArray {
+    public static void main(String ar[]){
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++)    arr[i]=sc.nextInt();
+
+        maxArraySum(arr,n);
+    }
 }
