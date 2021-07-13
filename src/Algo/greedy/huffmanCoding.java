037b87639f1ed96824847582498ecaba0a48c087
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-13 14:08:27 +0530

    Update huffmanCoding.java

--- src/Algo/greedy/huffmanCoding.java
+++ src/Algo/greedy/huffmanCoding.java
@@ -10,7 +10,8 @@ public class huffmanCoding {
         int n=sc.nextInt();
         sc.nextLine();
 
-        String[] ch=sc.nextLine().split(" ");
+        String s=sc.nextLine();
+        char ch[]=s.toCharArray();
 
         int[] freq=new int[n];
         for(int i=0;i<n;i++)  freq[i]=sc.nextInt();
@@ -18,8 +19,31 @@ public class huffmanCoding {
         buildHuff(ch,freq,n);
     }
 
-    static void buildHuff(String[] ch,int[] freq,int len){
+    static void buildHuff(char[] ch,int[] freq,int len){
         PriorityQueue<HuffmanNode> pq=new PriorityQueue<>(len,new myComparator());
+
+        for(int i=0;i<len;i++){
+            HuffmanNode hf=new HuffmanNode();
+
+            hf.c=ch[i];
+            hf.data=freq[i];
+
+            hf.left=null;   hf.right=null;
+
+            pq.add(hf);
+        }
+
+        HuffmanNode root=null;
+
+        while(pq.size()>1){
+
+        }
+
+        printHuff(root,"");
+    }
+
+    static void printHuff(HuffmanNode root,String s){
+        
     }
 }
 class HuffmanNode{
