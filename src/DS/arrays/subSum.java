ea2613d20bbdd2c0d9a86579ac89281ab854d56c
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-25 13:20:59 +0530

    Create subSum.java

---
+++ src/DS/arrays/subSum.java
@@ -0,0 +1,28 @@
+package DS.arrays;
+
+
+import java.util.Scanner;
+
+//sub array maximum sum;
+public class subSum {
+    private static StringBuilder sb=new StringBuilder();
+    public static void main(String ar[]){
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++)    arr[i]=sc.nextInt();
+
+        getSum(arr,n);
+    }
+    static void getSum(int[] arr,int len){
+        int maxSumyt=0,maxSum=Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++){
+            
+        }
+    }
+
+}
