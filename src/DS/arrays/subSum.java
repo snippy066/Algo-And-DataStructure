d928d1031e5cff628d1cd2b491fb3ade67d8d885
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-25 13:24:21 +0530

    Update subSum.java

--- src/DS/arrays/subSum.java
+++ src/DS/arrays/subSum.java
@@ -8,21 +8,29 @@ public class subSum {
     private static StringBuilder sb=new StringBuilder();
     public static void main(String ar[]){
         Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0) {
 
-        int n=sc.nextInt();
+            int n = sc.nextInt();
 
-        int[] arr=new int[n];
+            int[] arr = new int[n];
 
-        for(int i=0;i<n;i++)    arr[i]=sc.nextInt();
+            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
 
-        getSum(arr,n);
+            getSum(arr, n);
+        }
+        System.out.println(sb.toString());
     }
     static void getSum(int[] arr,int len){
         int maxSumyt=0,maxSum=Integer.MIN_VALUE;
 
-        for(int i=0;i<n;i++){
-            
+        for(int i=0;i<len;i++){
+            maxSumyt+=arr[i];
+            maxSum=Math.max(maxSum,maxSumyt);
+            if(maxSumyt<0)
+                maxSumyt=0;
         }
+        sb.append(maxSum+"\n");
     }
 
 }
