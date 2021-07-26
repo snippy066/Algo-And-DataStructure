7b4abcba818a8c93889109e83d367652a6d762a0
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-07-26 15:13:26 +0530

    Update kruskalMST.java

--- src/Algo/greedy/kruskalMST.java
+++ src/Algo/greedy/kruskalMST.java
@@ -10,6 +10,40 @@ public class kruskalMST {
         int V=in.nextInt();
 
         Graph gr=new Graph(V,E);
+
+        /* graph 
+                 10
+            0--------1
+            |  \     |
+           6|   5\   |15
+            |      \ |
+            2--------3
+                4       */
+
+        // add edge 0-1
+        gr.ed[0].src = 0;
+        gr.ed[0].dest = 1;
+        gr.ed[0].weight = 10;
+
+        // add edge 0-2
+        gr.ed[1].src = 0;
+        gr.ed[1].dest = 2;
+        gr.ed[1].weight = 6;
+
+        // add edge 0-3
+        gr.ed[2].src = 0;
+        gr.ed[2].dest = 3;
+        gr.ed[2].weight = 5;
+
+        // add edge 1-3
+        gr.ed[3].src = 1;
+        gr.ed[3].dest = 3;
+        gr.ed[3].weight = 15;
+
+        // add edge 2-3
+        gr.ed[4].src = 2;
+        gr.ed[4].dest = 3;
+        gr.ed[4].weight = 4;
     }
 }
 class Graph{
