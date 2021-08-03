d5689bd4a3e023db45e1d0dbf4088119481aea48
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-03 22:33:18 +0530

    Update loopDete.java

--- src/DS/LinkedList/loopDete.java
+++ src/DS/LinkedList/loopDete.java
@@ -1,4 +1,40 @@
+//loop detection in singly linked by floyd's cycle finding algo
 package DS.LinkedList;
 
+
 public class loopDete {
+    public static void main(String ar[]){
+        linkedList li=new linkedList();
+
+        li.push(10);
+        li.push(20);
+        li.push(30);
+        li.push(40);
+
+        li.head.next.next.next.next=li.head;        //created loop for testing
+
+        testLoop();
+    }
+
+    static void testLoop(){
+
+    }
+}
+
+class linkedList{
+    Node head;
+    static void push(int val){
+
+    }
+
+    class Node{
+        int data;
+        Node next;
+
+        Node(int d){
+            data=d;
+            next=null;
+        }
+    }
+
 }
