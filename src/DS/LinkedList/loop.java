b278000bc4139ddb42e2b41b96615d772410c35a
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-03 21:48:30 +0530

    linked list implementation and searching element

---
+++ src/DS/LinkedList/loop.java
@@ -0,0 +1,23 @@
+package DS.LinkedList;
+
+
+//searching element in singly linked list
+public class loop {
+    Node head;
+    public static void main(String ar[]){
+
+        loop li=new loop();
+
+
+    }
+}
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+
