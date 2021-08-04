c99e7c68189fbf106ed96ca67336c03a91f850ff
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-04 13:32:49 +0530

    Update midEle.java

--- src/DS/LinkedList/midEle.java
+++ src/DS/LinkedList/midEle.java
@@ -17,7 +17,15 @@ public class midEle {
     }
 
     static int printMid(linkedList list,linkedList.Node root){
-     return  root.data;
+        if(root==null)
+            return -1;
+        linkedList.Node slow_p=root,fast_p=root;
+
+        while(slow_p!=null && fast_p!=null && fast_p.next!=null)   {
+            slow_p=slow_p.next;
+            fast_p=fast_p.next.next;
+        }
+        return  slow_p.data;
     }
 }
 
