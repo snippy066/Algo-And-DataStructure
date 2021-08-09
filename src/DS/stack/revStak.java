525a7ed467aea0b25848b4d98d404b0935e7bc5f
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-09 22:10:58 +0530

    Create revStak.java

---
+++ src/DS/stack/revStak.java
@@ -0,0 +1,21 @@
+//package DS.stack;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class revStak {
+    public static void main(String ar[]){
+        List<Integer> li= Arrays.asList();
+
+        Stack<Integer> st=new Stack<>();
+        st.addAll(li);
+
+        System.out.println("original array: "+st);
+        rev(st);
+        System.out.println("reversed stack: "+st);
+    }
+    static void rev(Stack<Integer> st){
+        
+    }
+}
