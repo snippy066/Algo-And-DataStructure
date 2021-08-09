ea1441345b4b3a55b6f3ba5bba4e9ea43abbc06b
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-09 14:08:17 +0530

    Update minStack.java

--- src/DS/stack/minStack.java
+++ src/DS/stack/minStack.java
@@ -1,11 +1,13 @@
 //package DS.stack;
 //returning minimum element of stack while maintaining it's complexity
 
+import com.sun.javaws.jnl.LaunchSelection;
+
 import java.util.Stack;
 
 public class minStack {
     public static void main(String ar[]){
-        MinStack ms=new MinStack();
+        minimumStack ms=new minimumStack();
         ms.push(6);
         System.out.println(ms.min());    // prints 6
 
@@ -35,28 +37,44 @@ public class minStack {
     }
 }
 
-class MinStack{
+class minimumStack {
     private Stack<Integer> st;
     private Stack<Integer> aux;
-    MinStack(){
-
+    minimumStack(){
+        st=new Stack<>();
+        aux=new Stack<>();
     }
     void push(int d){
-
+        st.push(d);
+        if(aux.empty() || (aux.peek()>=d))
+            aux.push(d);
     }
     int pop(){
+        if(st.isEmpty()){
+            System.out.println("Uderflow");
+            return -1;
+        }
+        int tp=st.pop();
 
+        if(aux.peek()==tp)  aux.pop();
+
+        return tp;
     }
     int min(){
+        if(aux.isEmpty()){
+            System.out.println("Underflow");
+            return -1;
+        }
 
+        return aux.peek();
     }
     int size(){
-
+        return st.size();
     }
     int peek(){
-
+        return  st.size();
     }
-    boolean empty(){
-        
+    public boolean empty(){
+        return st.isEmpty();
     }
 }
