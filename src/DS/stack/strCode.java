b6b50b95f4246e1345846bf39568cc5eddef95d6
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-09 23:03:28 +0530

    Create strCode.java

---
+++ src/DS/stack/strCode.java
@@ -0,0 +1,10 @@
+//package DS.stack;
+
+public class strCode {
+    public static void main(String ar[]){
+        // input sequence
+        String st = "IDIDII";
+
+        System.out.println("The minimum number is " + decode(st));
+    }
+}
