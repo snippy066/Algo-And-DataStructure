d5852509a44566d02f707d66b7e566a39e8dfc0a
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-09 22:30:16 +0530

    Create dupliPra.java

---
+++ src/DS/stack/dupliPra.java
@@ -0,0 +1,24 @@
+package DS.stack;
+
+import java.util.Stack;
+
+public class dupliPra {
+    public static void main(String ar[]){
+        String exp = "((x+y))";        // assumes valid expression
+
+        if (duplicateParenthesis(exp)) {
+            System.out.println("The expression has duplicate parenthesis.");
+        }
+        else {
+            System.out.println("The expression does not have duplicate parenthesis");
+        }
+    }
+    static boolean duplicateParenthesis(String ex){
+        Stack<Character> st=new Stack<>();
+
+        for(Character c:ex.toCharArray()){
+            if(c!=')')  st.push(c);
+        }
+        return false;
+    }
+}
