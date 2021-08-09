b5fd3189b726a581d1c5de195534652596dead7a
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-09 22:17:59 +0530

    reversing stack
    reversing stack without using auxiliary space. using only push and pop methods

--- src/DS/stack/revStak.java
+++ src/DS/stack/revStak.java
@@ -6,12 +6,12 @@ import java.util.Stack;
 
 public class revStak {
     public static void main(String ar[]){
-        List<Integer> li= Arrays.asList();
+        List<Integer> li= Arrays.asList(1,2,3,4,5,6);
 
         Stack<Integer> st=new Stack<>();
         st.addAll(li);
 
-        System.out.println("original array: "+st);
+        System.out.println("original stack: "+st);
         rev(st);
         System.out.println("reversed stack: "+st);
     }
@@ -23,10 +23,18 @@ public class revStak {
 
         rev(st);
 
-        revInst(st,top);
+        instAtBtm(st,top);
     }
 
-    static void revInst(Stack<Integer> st,int key){
-        st.push(key);
+    static void instAtBtm(Stack<Integer> st,int key){
+        if(st.isEmpty()){
+            st.push(key);
+            return;
+        }
+
+        int top=st.pop();
+
+        instAtBtm(st,key);
+        st.push(top);
     }
 }
