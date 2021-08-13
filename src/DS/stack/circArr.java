fdcf45f5636137dda66e53612df86f224faa44d9
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-13 14:30:25 +0530

    Update circArr.java

--- src/DS/stack/circArr.java
+++ src/DS/stack/circArr.java
@@ -1,5 +1,32 @@
 //package DS.stack;
 //finding next greater element in circular array
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class circArr {
+    public static void main(String ar[]){
+        int[] arr = { 2, 7, 3, 5, 4, 6, 8 };
+
+        int[] result = findNextG(arr);
+        System.out.println(Arrays.toString(result));
+    }
+
+    static int[] findNextG(int[] arr) {
+        int len = arr.length;
+        int[] rs = new int[len];
+        Arrays.fill(rs, -1);  //so that if greater element not found it returns -1
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < len; i++) {
+
+            while (!st.isEmpty() && arr[st.peek()] < arr[i]) {
+                rs[st.pop()] = arr[i];
+            }
+            //current index
+            st.push(i);
+
+        }
+    }
 }
