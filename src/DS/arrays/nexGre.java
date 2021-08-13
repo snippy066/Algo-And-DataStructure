f4dd626f8c36f281e2f3407f0049388bc5915f39
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-13 14:15:15 +0530

    Update nexGre.java

--- src/DS/arrays/nexGre.java
+++ src/DS/arrays/nexGre.java
@@ -1,4 +1,21 @@
-package DS.arrays;
+//package DS.arrays;
+//finding next greater element in array
 
+import java.util.Arrays;
+
+/*
+Input:  [2, 7, 3, 5, 4, 6, 8]
+Output: [7, 8, 5, 6, 6, 8, -1]
+
+Input:  [5, 4, 3, 2, 1]
+Output: [-1, -1, -1, -1, -1] //no greater element found next
+
+ */
 public class nexGre {
+    public static void main(String ar[]){
+        int[] arr = { 2, 7, 3, 5, 4, 6, 8 };
+
+        int[] result = findNextG(arr);
+        System.out.println(Arrays.toString(result));
+    }
 }
