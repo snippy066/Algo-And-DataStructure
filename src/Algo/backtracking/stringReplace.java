89bfce08f909599b5d1ebfbea9bb8229a5307cdb
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-16 18:33:26 +0530

    Update stringReplace.java

--- src/Algo/backtracking/stringReplace.java
+++ src/Algo/backtracking/stringReplace.java
@@ -8,7 +8,19 @@ public class stringReplace {
         printAllCombinations(pattern, 0);
     }
 
-    static void printAllCombinations(char[] ch,int i){
-        
+    static void printAllCombinations(char[] patter,int i){
+        if(i==patter.length){
+            System.out.println(patter.toString());
+            return;
+        }
+
+        if(patter[i]=='?'){
+            for(char ch=0;ch<=1;ch++){
+                patter[i]=ch;
+                printAllCombinations(patter,i+1);
+                patter[i]='?';  //backtracking
+            }
+
+        }
     }
 }
