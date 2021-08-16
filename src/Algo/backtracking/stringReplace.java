4fcb53c2d73188f9b32770937318836e36ae0299
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-16 18:28:37 +0530

    Create stringReplace.java

---
+++ src/Algo/backtracking/stringReplace.java
@@ -0,0 +1,14 @@
+//package Algo.backtracking;
+//string replacement using backtracking and recursion
+
+
+public class stringReplace {
+    public static void main(String ar[]){
+        char[] pattern = "1?11?00?1?".toCharArray();
+        printAllCombinations(pattern, 0);
+    }
+
+    static void printAllCombinations(char[] ch,int i){
+        
+    }
+}
