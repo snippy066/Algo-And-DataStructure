75740dd9ad827ace929993bfb61946f294254648
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-20 22:10:33 +0530

    Update levelOrd.java

--- src/DS/queue/levelOrd.java
+++ src/DS/queue/levelOrd.java
@@ -1,8 +1,8 @@
 //package DS.queue;
-//level order traversal of binary tree
+//level order traversal of binary tree using queue
 
 public class levelOrd {
     public static void main(String ar[]){
-        
+
     }
 }
