657d589891f36e26207cabd811fe316fdebbb42a
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-20 22:11:20 +0530

    Update levelTravel.java

--- src/DS/BinaryTree/levelTravel.java
+++ src/DS/BinaryTree/levelTravel.java
@@ -2,7 +2,20 @@
 //level order traversal of binary tree using recursion
 
 public class levelTravel {
+
+    class Node{
+
+    }
+    
     public static void main(String ar[]){
-        
+        Node root = new Node(15);
+        root.left = new Node(10);
+        root.right = new Node(20);
+        root.left.left = new Node(8);
+        root.left.right = new Node(12);
+        root.right.left = new Node(16);
+        root.right.right = new Node(25);
+
+        levelOrderTraversal(root);
     }
 }
