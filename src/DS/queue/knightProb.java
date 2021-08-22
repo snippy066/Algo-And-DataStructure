6c9661aa427aca8caf312b847113fca140901c28
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-22 21:02:29 +0530

    Update knightProb.java

--- src/DS/queue/knightProb.java
+++ src/DS/queue/knightProb.java
@@ -1,4 +1,10 @@
-package DS.queue;
+//package DS.queue;
+//knight's problem from going to one corner to another corner
 
 public class knightProb {
+    static int col[]={};
+    static int row[]={};
+    public static void main(String ar[]){
+
+    }
 }
