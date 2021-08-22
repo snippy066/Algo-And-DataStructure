b57774c4f60dab523f5fa917e5b201cdab2df611
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-22 21:03:24 +0530

    Update knightProb.java

--- src/DS/queue/knightProb.java
+++ src/DS/queue/knightProb.java
@@ -2,8 +2,8 @@
 //knight's problem from going to one corner to another corner
 
 public class knightProb {
-    static int col[]={};
-    static int row[]={};
+    static int col[]={2,2,-2,-2,1,1,-1,-1};
+    static int row[]={1,-1,1,-1,2,-2,2,-2};
     public static void main(String ar[]){
 
     }
