8cfcc3f24afd65751b7b541bb1ad5128c27c38ce
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-23 19:46:33 +0530

    Update knightProb.java

--- src/DS/queue/knightProb.java
+++ src/DS/queue/knightProb.java
@@ -1,20 +1,15 @@
 //package DS.queue;
 //knight's problem from going to one corner to another corner
 
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Queue;
+
 public class knightProb {
     static StringBuilder sb=new StringBuilder();
     static int col[]={2,2,-2,-2,1,1,-1,-1};
     static int row[]={1,-1,1,-1,2,-2,2,-2};
-    public static void main(String ar[]){
-            int N=8;   //chessboard size
-            node sour=new node(0,7);
-            node dest=new node(7,0);
 
-            sb.append("No. of Steps required to reach destination : ");
-            findSteps(sour,dest,N);
-
-            System.out.println();
-    }
     static class node{
         int dist,x,y;
 
@@ -29,4 +24,34 @@ public class knightProb {
             dist=d;
         }
     }
+
+    public static void main(String ar[]){
+            int N=8;   //chessboard size
+            node sour=new node(0,7);
+            node dest=new node(7,0);
+
+            sb.append("No. of Steps required to reach destination : ");
+            findSteps(sour,dest,N);
+
+            System.out.println();
+    }
+
+    void findSteps(node s,node d,int size){
+        HashSet<node> hs=new HashSet<>();
+
+        Queue<node> q=new ArrayDeque<>();
+
+        q.add(s);
+
+        while(!q.isEmpty()){
+            node newNode =q.poll();
+
+            int x=newNode.x;
+            int y=newNode.y;
+            int dis=newNode.dist;
+
+            
+        }
+    }
+
 }
