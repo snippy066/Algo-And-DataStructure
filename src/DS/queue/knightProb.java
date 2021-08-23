b2788f237634146418ff973ea9d6829dbe7c469a
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-23 19:44:01 +0530

    Update knightProb.java

--- src/DS/queue/knightProb.java
+++ src/DS/queue/knightProb.java
@@ -2,13 +2,18 @@
 //knight's problem from going to one corner to another corner
 
 public class knightProb {
+    static StringBuilder sb=new StringBuilder();
     static int col[]={2,2,-2,-2,1,1,-1,-1};
     static int row[]={1,-1,1,-1,2,-2,2,-2};
     public static void main(String ar[]){
+            int N=8;   //chessboard size
             node sour=new node(0,7);
             node dest=new node(7,0);
 
+            sb.append("No. of Steps required to reach destination : ");
+            findSteps(sour,dest,N);
 
+            System.out.println();
     }
     static class node{
         int dist,x,y;
