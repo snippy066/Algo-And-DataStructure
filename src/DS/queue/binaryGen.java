ab95f77fda356d6a0e411411117303b9e0af9fdd
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-23 23:26:37 +0530

    Create binaryGen.java

---
+++ src/DS/queue/binaryGen.java
@@ -0,0 +1,17 @@
+//package DS.queue;
+//binary number generation from 1 to n
+
+import java.util.Scanner;
+
+public class binaryGen {
+    public static void main(String ar[]){
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        genBinary(n);
+        System.out.println(sb.toString());
+    }
+
+    
+}
