36081c9beb39d0ab825046412aa5d17ead26728b
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-23 19:35:33 +0530

    Update knightProb.java

--- src/DS/queue/knightProb.java
+++ src/DS/queue/knightProb.java
@@ -5,6 +5,17 @@ public class knightProb {
     static int col[]={2,2,-2,-2,1,1,-1,-1};
     static int row[]={1,-1,1,-1,2,-2,2,-2};
     public static void main(String ar[]){
+            node sour=new node(0,7);
+            node dest=new node(7,0);
 
+
+    }
+    static class node{
+        int dist;
+        int x,y;
+        node(int x,int y){
+            this.x=x;
+            this.y=y;
+        }
     }
 }
