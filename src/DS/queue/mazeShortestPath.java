3e793ace529bbdce9884b6132ec2f7fa1c54a8ce
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-23 21:40:14 +0530

    Update mazeShortestPath.java

--- src/DS/queue/mazeShortestPath.java
+++ src/DS/queue/mazeShortestPath.java
@@ -6,7 +6,11 @@ import java.util.Queue;
 
 public class mazeShortestPath {
 
-    class Node{
+    static int N=10;
+    static int[] row={1,-1,0,0};
+    static int[] col={0,0,1,-1};
+
+    static class Node{
         int x,y,dist;
         Node(int x,int y, int dist){
             this.x=x;
@@ -37,7 +41,15 @@ public class mazeShortestPath {
 
     static int shortPath(int[][] maze,int i,int j,int x,int y){
         Queue<Node> q=new ArrayDeque<>();
-        
+        boolean[][] visited=new boolean[N][N];
+
+        q.add(new Node(i,j,0));
+        visited[i][j]=true;
+
+        while(!q.isEmpty()){
+            
+        }
+
         return -1;
     }
 
