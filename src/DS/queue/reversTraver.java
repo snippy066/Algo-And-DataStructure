8d9a422e77b7c40a6f14a53ce387a5c5e95292f2
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-23 23:49:17 +0530

    Update reversTraver.java

--- src/DS/queue/reversTraver.java
+++ src/DS/queue/reversTraver.java
@@ -3,7 +3,27 @@
 
 
 public class reversTraver {
-    public static void main(String ar[]){
+    static StringBuilder sb=new StringBuilder();
 
+    class Node{
+        int data;
+        Node left,right;
+
+        Node(int d){
+            data=d;
+            left=right=null;
+        }
+    }
+
+    public static void main(String ar[]){
+        Node root = new Node(15);
+        root.left = new Node(10);
+        root.right = new Node(20);
+        root.left.left = new Node(8);
+        root.left.right = new Node(12);
+        root.right.left = new Node(16);
+        root.right.right = new Node(25);
+
+        System.out.println("reverse of the binary tree is : "+ sb.toString());
     }
 }
