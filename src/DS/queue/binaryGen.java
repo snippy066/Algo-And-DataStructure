f15663f5e90946e234d3a9157cad6bac483f8bbb
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-23 23:32:04 +0530

    binary numbers between 1 to `n` using a queue

--- src/DS/queue/binaryGen.java
+++ src/DS/queue/binaryGen.java
@@ -1,6 +1,8 @@
 //package DS.queue;
-//binary number generation from 1 to n
-
+/*binary number generation from 1 to n
+ex :
+    1 10 11 100 101 110 111 1000 1001 1010 1011 1100 1101 1110 1111 10000
+ */
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.Scanner;
@@ -20,14 +22,14 @@ public class binaryGen {
         Queue<String> q=new ArrayDeque<>();
 
         q.add("1");
-
-        while(!q.isEmpty()){
+        int i=1;
+        while(i++<=n){
             String s=q.poll();
 
             q.add(s+"0");
             q.add(s+"1");
 
-            sb.append(s);
+            sb.append(s+" ");
         }
     }
 }
