e96eafd71681591fca7f3aafafb345bfd5030184
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-24 18:53:03 +0530

    Update spiralOrder.java

--- src/DS/queue/spiralOrder.java
+++ src/DS/queue/spiralOrder.java
@@ -12,8 +12,26 @@
  */
 
 public class spiralOrder {
-    
-    public static void main(String ar[]){
 
+    static class Node{
+        int data;
+        Node left, right;
+
+        Node(int d){
+            data=d;
+            left=right=null;
+        }
+    }
+
+    public static void main(String ar[]){
+        Node root = new Node(15);
+        root.left = new Node(10);
+        root.right = new Node(20);
+        root.left.left = new Node(8);
+        root.left.right = new Node(12);
+        root.right.left = new Node(16);
+        root.right.right = new Node(25);
+
+        spiralOrderTraversal(root);
     }
 }
