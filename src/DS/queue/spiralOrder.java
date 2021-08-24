7035ce283d35c652493c474937314326e517e9e4
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-24 18:51:50 +0530

    Update spiralOrder.java

--- src/DS/queue/spiralOrder.java
+++ src/DS/queue/spiralOrder.java
@@ -1,8 +1,19 @@
 //package DS.queue;
 //spiral order traversal of binary tree
 
+/*
+                    1
+                 /      \
+                2         3
+              /   \     /    \
+             4     5   6      7
+
+        order : (1,2,3,7,6,5,4) or (1,3,2,4,5,6,7)
+ */
+
 public class spiralOrder {
+    
     public static void main(String ar[]){
-        
+
     }
 }
