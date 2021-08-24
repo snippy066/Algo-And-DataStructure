3db253d2921788006ca6db6c34a36f6e3d435fcf
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-24 20:26:54 +0530

    Update spiralOrder.java

--- src/DS/queue/spiralOrder.java
+++ src/DS/queue/spiralOrder.java
@@ -12,9 +12,12 @@
  */
 
 import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.Deque;
+//import java.util.Queue;
 
 public class spiralOrder {
+
+    static StringBuilder sb=new StringBuilder();
 
     static class Node{
         int data;
@@ -27,22 +30,55 @@ public class spiralOrder {
     }
 
     public static void main(String ar[]){
-        Node root = new Node(15);
-        root.left = new Node(10);
-        root.right = new Node(20);
-        root.left.left = new Node(8);
-        root.left.right = new Node(12);
-        root.right.left = new Node(16);
-        root.right.right = new Node(25);
-        root.left.left.left = new Node(20);
-        root.right.right.right = new Node(30);
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        root.left.left.left = new Node(8);
+        root.right.right.right = new Node(9);
 
         spiralOrderTraversal(root);
     }
 
     static void spiralOrderTraversal(Node root){
-        Queue<Node> q=new ArrayDeque<>();
+        Deque<Node> q=new ArrayDeque<>();
 
+        q.add(root);
+        boolean flag=false;
+        while(!q.isEmpty()){
+            int size=q.size();
+
+            //left to right
+            if(flag){
+                while(size-->0){
+                    Node node=q.pollFirst();
+                    sb.append(node.data+" ");
+
+                    if(node.left!=null)
+                        q.addLast(node.left);
+                    if(node.right!=null)
+                        q.addLast(node.right);
+                }
+            }
+            //right to left
+            else{
+                while(size-->0) {
+                    Node node = q.pollLast();
+                    sb.append(node.data + " ");
+                    
+                    if (node.right != null)
+                        q.addFirst(node.right);
+                    if (node.left != null)
+                        q.addFirst(node.left);
+
+                }
+            }
+            flag=!flag;
+            sb.append("\n");
+        }
 
     }
 }
