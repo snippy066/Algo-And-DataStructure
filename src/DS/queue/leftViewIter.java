2a384f93a25a512e3172487ba5fd734fdf7f8743
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-24 22:18:21 +0530

    Create leftViewIter.java

---
+++ src/DS/queue/leftViewIter.java
@@ -0,0 +1,17 @@
+//left view of binary tree using queue iterative approach
+//package DS.queue;
+
+public class leftViewIter {
+    public static void main(String ar[]){
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.right = new Node(4);
+        root.right.left = new Node(5);
+        root.right.right = new Node(6);
+        root.right.left.left = new Node(7);
+        root.right.left.right = new Node(8);
+
+        leftView(root);
+    }
+}
