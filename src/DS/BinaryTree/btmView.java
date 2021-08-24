dce067891b0465d2b129b0bd2492bb4db9de2b37
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-08-24 19:46:05 +0530

    Create btmView.java

---
+++ src/DS/BinaryTree/btmView.java
@@ -0,0 +1,14 @@
+//package DS.BinaryTree;
+//bottom view of a binary tree
+
+/*
+                    1
+                 /      \
+                2         3
+              /   \     /    \
+             4     5   6      7
+
+        view : 
+ */
+public class btmView {
+}
