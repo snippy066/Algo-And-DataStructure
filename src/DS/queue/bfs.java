71b73379ef60346760d6ad655c95fe1ec6d16645
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-09-01 22:50:21 +0530

    Update bfs.java

--- src/DS/queue/bfs.java
+++ src/DS/queue/bfs.java
@@ -2,16 +2,10 @@
 //breadth first search using iterative method with help of queue
 
 import java.util.Arrays;
+import java.util.List;
 
 public class bfs {
 
-    class Edge{
-        int src,dest;
-        Edge(int s,int d){
-            src=s;
-            dest=d;
-        }
-    }
     public static void main(String ar[]){
         List<Edge> edges = Arrays.asList(
                 new Edge(1, 2), new Edge(1, 3), new Edge(1, 4),
@@ -19,5 +13,25 @@ public class bfs {
                 new Edge(5, 10), new Edge(4, 7), new Edge(4, 8),
                 new Edge(7, 11), new Edge(7, 12)
         );
+
+        int n=15;   //number of nodes
+
+        Graph g=new Graph(edges,n);
+
+
     }
+}
+class Edge{
+    int src,dest;
+    Edge(int s,int d){
+        src=s;
+        dest=d;
+    }
+}
+
+class Graph{
+    Graph(List<Edge> ed, int N){
+
+    }
+
 }
