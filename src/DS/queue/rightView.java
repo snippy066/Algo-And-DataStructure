c9e9a8fcbefdbb8c15002ddd9f2aac3ef30522ab
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-09-03 09:14:26 +0530

    Create rightView.java

---
+++ src/DS/queue/rightView.java
@@ -0,0 +1,24 @@
+//package DS.queue;
+//right view of a binary tree
+
+/*
+            1
+          /   \
+         2     3
+        /     / \
+        4     5  6
+                 /
+                 7
+
+
+       right view =( 1,2,4,7);
+
+ */
+public class rightView {
+    public static void main(String ar[]){
+        linkedList.Node root=new linkedList.Node(1);
+
+    }
+}
+
+
