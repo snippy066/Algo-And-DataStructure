887e97ca141d5dba62163ccdc08e41aa47e530cd
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-09-07 20:48:43 +0530

    top view of binary tree

--- src/DS/BinaryTree/topView.java
+++ src/DS/BinaryTree/topView.java
@@ -2,6 +2,7 @@
 //top view of binary tree
 
 import java.util.Map;
+import java.util.TreeMap;
 
 public class topView {
     static class Node{
@@ -22,10 +23,10 @@ public class topView {
             this.second=second;
         }
 
-        Pair<U,V> of(U x,V y){
+        static<U,V> Pair<U,V> Of(U x,V y){
             return new Pair<>(x,y);
         }
-        
+
     }
 
     public static void main(String ar[]){
@@ -37,13 +38,31 @@ public class topView {
         root.right.right = new Node(6);
         root.right.left.left = new Node(7);
         root.right.left.right = new Node(8);
+        root.right.left.left.left = new Node(9);
 
         printTop(root);
     }
 
     static void printTop(Node root){
-        Map<Integer,Pair<Integer,Integer>> m=new Map<Integer, Pair<Integer, Integer>>();
+        Map<Integer,Pair<Integer,Integer>> m=new TreeMap<>();
 
+        printTop(root,0,0,m);
+
+        for(Pair<Integer,Integer> it : m.values()){
+            System.out.print(it.firs+" ");
+        }
+
+    }
+
+    static void printTop(Node head,int dist,int level,Map<Integer,Pair<Integer,Integer>> m){
+        if(head==null)
+            return;
+
+        if(!m.containsKey(dist)||level<m.get(dist).second)
+            m.put(dist,Pair.Of(head.data,level));
+
+        printTop(head.left,dist-1,level+1,m);
+        printTop(head.right,dist+1,level+1,m);
     }
 
 }
