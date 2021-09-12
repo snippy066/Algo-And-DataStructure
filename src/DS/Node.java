b5f7c8262785c01711e38fac2bb035391599ba4f
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-09-12 07:02:43 +0530

    Create Node.java

---
+++ src/DS/Node.java
@@ -0,0 +1,10 @@
+package DS;
+
+public class Node {
+        public int data;
+       public Node left,right;
+        public Node(int data){
+            this.data=data;
+            left=right=null;
+        }
+}
