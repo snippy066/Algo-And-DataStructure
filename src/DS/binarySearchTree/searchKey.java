4539625438bd361beb34de49de5f113b60eb20a6
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-09-15 13:27:38 +0530

    binary tree search key

--- src/DS/binarySearchTree/searchKey.java
+++ src/DS/binarySearchTree/searchKey.java
@@ -1,16 +1,19 @@
 //package DS.binarySearchTree;
 //search key in a binary search tree
 
-import DS.Node;
+
 
 public class searchKey {
-    public  static Node insert(Node root, int d){
-        if(root==null) new Node(d);
-        if(d>root.data) insert(root.right,d);
-        else
-            insert(root.left,d);
-        return root;
+
+    static class Node{
+        int data;
+        Node left = null, right = null;
+
+        Node(int data) {
+            this.data = data;
+        }
     }
+
     public static void main(String ar[]){
         int[] keys = { 15, 10, 20, 8, 12, 16, 25 };
 
@@ -19,6 +22,13 @@ public class searchKey {
             root = insert(root, key);
         }
         search(root,25,null);
+    }
+    public  static Node insert(Node root, int d){
+        if(root==null) return new Node(d);
+        if(d>root.data) insert(root.right,d);
+        else
+            insert(root.left,d);
+        return root;
     }
     static void search(Node root,int d,Node parent){
         if(root==null){
@@ -29,7 +39,7 @@ public class searchKey {
             if(parent==null){
                 System.out.println("value found at root" +d);
             }
-            if(parent.data>d){
+            else if(parent.data>d){
                 System.out.println("value found at left tree of "+parent.data);
             }
             else{
@@ -37,7 +47,8 @@ public class searchKey {
             }
             return;
         }
-        else if(root.data>d) search(root.left,d,root);
+        else if(root.data>d)
+            search(root.left,d,root);
         else
             search(root.right,d,root);
     }
