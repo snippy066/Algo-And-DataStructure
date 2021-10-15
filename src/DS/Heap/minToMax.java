90e384de750d2c93d14c75974dc5d4c9953a4048
Priyanka Thakur <priyankathakur221206@gmail.com>
2021-10-15 12:08:26 +0530

    Create minToMax.java

---
+++ src/DS/Heap/minToMax.java
@@ -0,0 +1,57 @@
+package DS.Heap;
+
+import java.util.Scanner;
+
+public class minToMax {
+
+    private int leftch(int i){
+        return 2*i+1;
+    }
+    private int rightch(int i){
+        return 2*i+2;
+    }
+    public static void main(String ar[]){
+
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++) arr[i]=sc.nextInt();
+        minToMax mm=new minToMax();
+        mm.maxHeap(arr);
+
+        System.out.println(arr.toString());
+    }
+
+    void maxHeap(int[] arr){
+        int i=(arr.length-2)/2;
+        while(i-->=0)
+            heapify(arr,arr.length,i)
+    }
+
+    void heapify(int[] arr,int size,int i){
+        int left=leftch(i);
+        int right=rightch(i);
+
+        int smallest=i;
+
+        if(left<size && arr[left]<arr[smallest]){
+            smallest=left;
+        }
+        if(right<size && arr[right]<arr[smallest]){
+            smallest=right;
+        }
+
+        if(smallest!=i){
+            swap(arr,i,smallest);
+            heapify(arr,size,smallest);
+        }
+    }
+
+    void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
