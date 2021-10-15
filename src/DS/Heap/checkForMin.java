1b6ac29d5311bc2d52ab4b3398d6e875502842f3
Priyanka Thakur <priyankathakur221206@gmail.com>
2021-10-15 11:49:39 +0530

    check for minimum heap

--- src/DS/Heap/checkForMin.java
+++ src/DS/Heap/checkForMin.java
@@ -19,6 +19,7 @@ public class checkForMin {
 
     static boolean checkHeap(int[] arr){
         for(int i=0;i<=(arr.length-2)/2;i++){
+
             if(arr[i]>arr[2*i+1] || ((2*i+2!=arr.length && arr[i]>arr[2*i+2])))
                 return false;
         }
